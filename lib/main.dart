1c84a369a410bdec5e858047f564c852f721ef14
laila <lkarimi@css.edu>
2025-11-23 20:30:16 -0600

    Sprint 2: Added JSON backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 void main() {
   runApp(BookCatalogApp());
@@ -18,21 +20,42 @@ class BookCatalogApp extends StatelessWidget {
   }
 }
 
-// Simple Book model
+// Book model class
 class Book {
   final String title;
   final String author;
 
-  Book(this.title, this.author);
+  Book({required this.title, required this.author});
+
+  factory Book.fromJson(Map<String, dynamic> json) {
+    return Book(
+      title: json["title"],
+      author: json["author"],
+    );
+  }
 }
 
-class BookListScreen extends StatelessWidget {
-  // Hardcoded sample book list for Sprint 1
-  final List<Book> books = [
-    Book("The Alchemist", "Paulo Coelho"),
-    Book("Dune", "Frank Herbert"),
-    Book("Percy Jackson", "Rick Riordan"),
-  ];
+class BookListScreen extends StatefulWidget {
+  @override
+  _BookListScreenState createState() => _BookListScreenState();
+}
+
+class _BookListScreenState extends State<BookListScreen> {
+  late Future<List<Book>> futureBooks;
+
+  @override
+  void initState() {
+    super.initState();
+    futureBooks = loadBooks();
+  }
+
+  // Load and parse the JSON file
+  Future<List<Book>> loadBooks() async {
+    final jsonString = await rootBundle.loadString('assets/books.json');
+    final List<dynamic> jsonData = json.decode(jsonString);
+
+    return jsonData.map((item) => Book.fromJson(item)).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,17 +63,33 @@ class BookListScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Book Catalog"),
       ),
-      body: ListView.builder(
-        itemCount: books.length,
-        itemBuilder: (context, index) {
-          final book = books[index];
-          return Card(
-            margin: EdgeInsets.all(12),
-            child: ListTile(
-              title: Text(book.title),
-              subtitle: Text(book.author),
-              trailing: Icon(Icons.arrow_forward_ios),
-            ),
+      body: FutureBuilder<List<Book>>(
+        future: futureBooks,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return Center(child: Text("Error loading data"));
+          }
+
+          final books = snapshot.data!;
+
+          return ListView.builder(
+            itemCount: books.length,
+            itemBuilder: (context, index) {
+              final book = books[index];
+
+              return Card(
+                margin: EdgeInsets.all(12),
+                child: ListTile(
+                  title: Text(book.title),
+                  subtitle: Text(book.author),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                ),
+              );
+            },
           );
         },
       ),
